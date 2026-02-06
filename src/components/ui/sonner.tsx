aadb83d77f7109229ba201af720c955775ad6dd0
Kacper Lechicki <klechicki.wrk@gmail.com>
2026-02-06 12:18:15 +0100

    feat: authorization logic v6

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -5,7 +5,22 @@ import { Toaster as Sonner } from 'sonner';
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = (props: ToasterProps) => {
-  return <Sonner theme="dark" richColors closeButton position="bottom-right" {...props} />;
+  return (
+    <Sonner
+      theme="dark"
+      richColors
+      closeButton
+      position="bottom-right"
+      visibleToasts={3}
+      gap={8}
+      toastOptions={{
+        style: {
+          marginBottom: '0px',
+        },
+      }}
+      {...props}
+    />
+  );
 };
 
 export { Toaster };
