e11a0123b8ce7c666d844ec89e86c83a85c2e477
Kacper Lechicki <klechicki.wrk@gmail.com>
2026-02-10 20:37:23 +0100

    fix: layout spacing

--- src/app/[locale]/(dashboard)/(main)/layout.tsx
+++ src/app/[locale]/(dashboard)/(main)/layout.tsx
@@ -5,7 +5,7 @@ import { BackButton } from '@/components/ui/back-button';
 export default function MainLayout({ children }: { children: ReactNode }) {
   return (
     <main className="flex-1">
-      <div className="container mx-auto px-6 pt-6 pb-8 sm:px-4 md:pt-10 lg:px-8">
+      <div className="container mx-auto px-6 pt-6 pb-20 sm:px-4 md:pt-10 lg:px-8">
         <div className="mb-6 md:mb-10 lg:hidden">
           <BackButton />
         </div>
